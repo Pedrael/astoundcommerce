af9b62e760a67bc43b4ce8b1521d6be9ab4f0c3d
Pedrael <mihail_krivic@mail.ru>
2021-03-19 17:29:13 +0300

    +3

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,21 @@
 let wContent = document.querySelector('.share-content');
 let showButton = document.querySelector('.js-open');
 let closeButton = document.querySelector('.js-close');
+let {Carousel} = require("latte-carousel");
+
+document.addEventListener('DOMContentLoaded', () => {
+    const carousel = new Carousel('#carousel', {
+        count: 1,
+        move: 1,
+        touch: true,
+        mode: "free",
+        buttons: false,
+        dots: false,
+        rewind: true,
+        autoplay: 0,
+        animation: 500
+    });
+});
 
 if(showButton && wContent && closeButton) {
     showButton.addEventListener('click', (e) => {
