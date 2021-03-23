f73d26bb6027fa6c768371b0b7fc217d940bbe86
Pedrael <mihail_krivic@mail.ru>
2021-03-23 19:42:07 +0300

    done

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,66 @@
-let wContent = document.querySelector('.share-content');
-let showButton = document.querySelector('.js-open');
-let closeButton = document.querySelector('.js-close');
-import Swiper from 'swiper/bundle';
-
-document.addEventListener('DOMContentLoaded', () => {
-    const swiper = new Swiper(".swiper-container", {
-        direction: 'vertical',
-        slidesPerView: 1,
-        slideClass: "swiper-slide",
-        autoplay: true,
-        loop: true
-    });
-});
-
-if(showButton && wContent && closeButton) {
-    showButton.addEventListener('click', (e) => {
-        if(e.target == showButton) {
-            wContent.style.opacity = 1;
-            wContent.style.zIndex = 1;
-        }
-    });
-
-    closeButton.addEventListener('click', (e) => {
-        if(e.currentTarget == closeButton) {
-            wContent.style.opacity = 0;
-            wContent.style.zIndex = -1;
-        }
-    });
+let right = function(cards, index) {
+    cards[index].style.display = "none";
+    cards.length - 1 == index ? index = 0 : index++;
+    cards[index].style.display = "flex";
+    return index;
 }
+
+let left = function(cards, index) {
+    cards[index].style.display = "none";
+    0 == index ? index = cards.length - 1 : index--;
+    cards[index].style.display = "flex";
+    return index;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    let wContent = document.querySelector('.share-content');
+    let showButton = document.querySelector('.js-open');
+    let closeButton = document.querySelector('.js-close');
+    let cWrapper = document.querySelector('.carousel');
+    let lBtn = document.querySelector('.left-btn');
+    let rBtn = document.querySelector('.right-btn');
+
+    if(cWrapper) {
+        let cards = cWrapper.querySelectorAll('.card');
+        let index = 0;
+        if(window.innerWidth < cWrapper.dataset.responsive)
+            cards.forEach((card, i) => {
+                if(i != index) {
+                    card.style.display = "none";
+                }
+            });
+        window.onresize = () => {
+            if(window.innerWidth < cWrapper.dataset.responsive)
+                cards.forEach((card, i) => {
+                    if(i != index) {
+                        card.style.display = "none";
+                    }
+                });
+            else cards.forEach((card) => {
+                card.style.display = "flex";
+            });
+        }
+        if(lBtn && rBtn) {
+            lBtn.addEventListener("click", () => index = left(cards, index));
+            rBtn.addEventListener("click", () => index = right(cards, index));
+        }
+    }
+
+    if(showButton && wContent && closeButton) {
+        showButton.addEventListener('click', (e) => {
+            if(e.target == showButton) {
+                wContent.style.opacity = 1;
+                wContent.style.zIndex = 1;
+            }
+        });
+
+        closeButton.addEventListener('click', (e) => {
+            if(e.currentTarget == closeButton) {
+                wContent.style.opacity = 0;
+                wContent.style.zIndex = -1;
+            }
+        });
+    }
+
+});
